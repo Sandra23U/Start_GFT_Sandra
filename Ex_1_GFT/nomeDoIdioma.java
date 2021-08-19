0ab7fc98ab9dbd742f11166651768f811b407b3d
Sandra de Oliveira Souza <66983974+Sandra23U@users.noreply.github.com>
2021-08-19 18:08:05 -0300

    Add files via upload

---
+++ Ex_1_GFT/nomeDoIdioma.java
@@ -0,0 +1,15 @@
+                                          import br.com.angeloakm.abstratas.Animal;
+
+
+public class nomeDoIdioma {
+
+        public static void main(String[] args) {
+        System.out.println("Olá Mundo!");
+        System.out.println("Hello World!");
+        System.out.println("Helló Világ!");
+         System.out.println("Ahoj Svete!");
+                                            
+    
+    
+    }
+}
